c6305dd3c5fa1442687a5aebaf460d2a2d42cea5
Anti-weakpasswords <deepside@hotmail.com>
2014-03-02 09:12:10 +0000

    remove tests

--- compile.bat
+++ compile.bat
@@ -1,6 +1,1 @@
 C:\MinGW\bin\gcc -O3 -I "c:\OpenSSL-Win32\include" -L "C:\OpenSSL-Win32" -leay32 pbkdf2_openssl.c -o pbkdf2
-
-C:\MinGW\bin\gcc -O3 -ffast-math -I "c:\OpenSSL-Win32\include" -L "C:\OpenSSL-Win32" -leay32 pbkdf2_openssl.c -o pbkdf2a
-
-C:\MinGW\bin\gcc -O3 -march=native -mtune=native -ffast-math -I "c:\OpenSSL-Win32\include" -L "C:\OpenSSL-Win32" -leay32 pbkdf2_openssl.c -o pbkdf2b
-
