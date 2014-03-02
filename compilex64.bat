d05f87872ddc2615a5cf6493171adc047018ec49
Anti-weakpasswords <deepside@hotmail.com>
2014-03-02 09:15:14 +0000

    Update compilex64.bat

--- compilex64.bat
+++ compilex64.bat
@@ -1,3 +1,5 @@
 rem this is the path to your OpenSSL binary installation
 rem you can find binary distributions for Windows at: https://www.openssl.org/related/binaries.html
+rem MinGW x86 (32-bit) or x64 (64-bit) is required to compile
+rem libeay32.dll from OpenSSL is required to run.
 gcc -O3 -I "c:\OpenSSL-Win64\include" -L "C:\OpenSSL-Win64" -leay32 pbkdf2_openssl.c -o pbkdf2
